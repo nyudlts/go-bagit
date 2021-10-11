7bfaa3abbd0355581274232bc33aa5f03628f24e
dmnyu <don.mennerich@nyu.edu>
2021-10-11 16:52:33 -0400

    adding tagmanifest to validation function

--- bag.go
+++ bag.go
@@ -3,6 +3,7 @@ package go_bagit
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 	"strings"
@@ -39,9 +40,15 @@ func ValidateBag(bagLocation string) error {
 		return err
 	}
 
+	tagmanifest := filepath.Join(bagLocation, "tagmanifest-sha256.txt")
+	if err := ValidateManifest(tagmanifest); err != nil {
+		return err
+	}
+
 	manifest := filepath.Join(bagLocation, "manifest-sha256.txt")
 	if err := ValidateManifest(manifest); err != nil {
 		return err
 	}
+	log.Printf("- INFO - %s is valid", bagLocation)
 	return nil
 }
--- oxum.go
+++ oxum.go
@@ -2,7 +2,6 @@ package go_bagit
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -10,7 +9,7 @@ import (
 )
 
 type Oxum struct {
-	Size int64
+	Size  int64
 	Count int
 }
 
@@ -20,7 +19,7 @@ func (o Oxum) String() string {
 
 func ParseOxumString(oxum string) (Oxum, error) {
 	o := Oxum{}
-	splitOxum  := strings.Split(oxum, ".")
+	splitOxum := strings.Split(oxum, ".")
 	var err error
 	o.Size, err = strconv.ParseInt(splitOxum[0], 10, 64)
 	if err != nil {
@@ -34,14 +33,11 @@ func ParseOxumString(oxum string) (Oxum, error) {
 	return o, nil
 }
 
-
 func ValidateOxum(bagLocation string, oxum string) error {
 	storedOxum, err := ParseOxumString(oxum)
 	if err != nil {
 		return err
 	}
-
-	log.Println(storedOxum.String())
 
 	calculatedOxum, err := CalculateOxum(bagLocation)
 	if err != nil {
@@ -59,7 +55,7 @@ func ValidateOxum(bagLocation string, oxum string) error {
 	return nil
 }
 
-func CalculateOxum(bagLocation string) (Oxum, error){
+func CalculateOxum(bagLocation string) (Oxum, error) {
 	dataDir := filepath.Join(bagLocation, "data")
 
 	var size int64 = 0
