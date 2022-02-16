239428ead132a5ac9a81da6502fe6204b0bd0d72
Don Mennerich <don.mennerich@nyu.edu>
2022-02-16 14:02:25 -0500

    adding function to get software agent

--- common.go
+++ common.go
@@ -1,7 +1,14 @@
 package go_bagit
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 var currentTime = time.Now()
 
 var libraryVersion = "v0.1.1-alpha"
+
+func GetSoftwareAgent() string {
+	return fmt.Sprintf("go-bagit %s <https://github.com/nyudlts/go-bagit>", libraryVersion)
+}
