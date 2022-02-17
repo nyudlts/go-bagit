f729f44bb4b61b7e24d9af6cf25e8539af337b56
Don Mennerich <don.mennerich@nyu.edu>
2022-02-17 08:34:30 -0500

    updating tests re: `https://github.com/sevein/go-bagit/commit/52f000b8e2f9ab9b6ef5bbd6c91379b26b9597d0`

--- common.go
+++ common.go
@@ -2,13 +2,27 @@ package go_bagit
 
 import (
 	"fmt"
+	"runtime/debug"
 	"time"
 )
 
 var currentTime = time.Now()
 
-var libraryVersion = "v0.1.1-alpha"
-
 func GetSoftwareAgent() string {
-	return fmt.Sprintf("go-bagit %s <https://github.com/nyudlts/go-bagit>", libraryVersion)
+	const mod = "github.com/nyudlts/go-bagit"
+
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return fmt.Sprintf("go-bagit <https://%s>", mod)
+	}
+
+	version := "(unknown)"
+	for _, dep := range info.Deps {
+		if dep.Path == mod {
+			version = dep.Version
+			break
+		}
+	}
+
+	return fmt.Sprintf("go-bagit %s <https://%s>", version, mod)
 }
---
+++ common_test.go
@@ -0,0 +1,7 @@
+package go_bagit
+
+import "testing"
+
+func TestGetSoftwareAgent(t *testing.T) {
+	t.Log(GetSoftwareAgent())
+}
